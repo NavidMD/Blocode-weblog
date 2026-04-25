09f30d6e12cac57c5ea0b18066114c010ca2528c
NavidMoeinDarbari <navidoomoeindarbar77@gmail.com>
2026-04-25 19:59:49 +0330

    feat: افزودن دکمه تم دارک و لایت به نوبار

--- UI/Blocode/src/app/core/components/navbar/navbar.ts
+++ UI/Blocode/src/app/core/components/navbar/navbar.ts
@@ -4,34 +4,9 @@ import { CategoryList } from '../../../features/category/category-list/category-
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, CategoryList],
+  imports: [CategoryList, RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
-  constructor(private router: Router) {}
-
-  optionChangeHandler(option: Event): void {
-    const selectElement = option.target as HTMLSelectElement;
-
-    let path = '';
-    switch (selectElement.value) {
-      case 'categories':
-        path = 'categories';
-        break;
-      case 'blogs':
-        path = 'blogs';
-        break;
-      case 'products':
-        path = 'products';
-        break;
-      default:
-        path = '';
-    }
-    if (path) {
-      this.router.navigate(['admin', path]).catch(() => {
-        this.router.navigate([''])
-      });
-    }
-  }
 }
