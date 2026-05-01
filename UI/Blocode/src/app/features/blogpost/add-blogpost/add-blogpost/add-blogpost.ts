fbc11d3041b78cab12c3558600827ec4f6306d7a
Navid Moein Darbari <navidoomoeindarbar77@gmail.com>
2026-05-01 11:04:32 +0430

    fit: افزودن متن های اعتبارسنچی به  فرم ایجاد مقاله

--- UI/Blocode/src/app/features/blogpost/add-blogpost/add-blogpost/add-blogpost.ts
+++ UI/Blocode/src/app/features/blogpost/add-blogpost/add-blogpost/add-blogpost.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-add-blogpost',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './add-blogpost.html',
   styleUrl: './add-blogpost.css',
 })
